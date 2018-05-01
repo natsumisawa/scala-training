ae18fda0b6c087657f7a21b8d9e309484ab278a5
澤なつみ <n2natsu1218@gmail.com>
2018-05-02 07:48:17 +0900

    :bug:

--- functor-day/Functor.scala
+++ functor-day/Functor.scala
@@ -1,7 +1,7 @@
 object Functor {
 
   // 愚直ver
-  def num2Month(num: Int): String = num match {
+  def number2Month(num: Int): String = num match {
     case 1 => "January"
     case 2 => "February"
     case 3 => "March"
@@ -18,8 +18,7 @@ object Functor {
   }
 
   def month2Initial(num: Int): Command = {
-    val month = num2Month(num)
-
+    val month = number2Month(num)
     if (month != null) new MonthCommand(month.substring(0, 3))
     else new NullCommand
   }
@@ -42,12 +41,12 @@ object Functor {
   }
 
   // Optionを使う
-  def num2Month2(num: Int): Option[String] =
+  def number2Month2(num: Int): Option[String] =
     Map(1 -> "January", 2 -> "February", 3 -> "March", 4 -> "April", 5 -> "May", 6 -> "June", 7 -> "July", 8 -> "August", 9 -> "September", 10 -> "October", 11 -> "November", 12 -> "December")
     .get(num)
 
   def month2Initial2(num: Int): Option[String] = {
-    (num2Month2(num) match {
+    (number2Month2(num) match {
       case Some(month) => Some(month.substring(0, 3))
       case None => None
     }) match {
@@ -58,38 +57,34 @@ object Functor {
 
   // Optionで高階関数を使う
   def month2Initial3(num: Int): String = {
-    num2Month2(num)
+    number2Month2(num)
       .map(_.substring(0, 3))
       .map(month => if (month == "May") month else s"$month.")
       .getOrElse(throw new RuntimeException("Illegal number"))
   }
 
   // Eitherを使う
-  def num2Month3(num: Int): Either[Exception, String] =
+  def number2Month3(num: Int): Either[Exception, String] =
     Map(1 -> "January", 2 -> "February", 3 -> "March", 4 -> "April", 5 -> "May", 6 -> "June", 7 -> "July", 8 -> "August", 9 -> "September", 10 -> "October", 11 -> "November", 12 -> "December")
       .get(num).toRight(new RuntimeException("Illegal number"))
 
   def month2Initial4(num: Int): String =
-    num2Month3(num)
+    number2Month3(num)
       .map(_.substring(0, 3))
       .map(month => if (month == "May") month else s"$month.")
       .getOrElse(throw new RuntimeException("Illegal number"))
 
-  // Validateを使う
-  import scalaz.Scalaz._
-  import scalaz._
+  def numbers2Month(num: Int*): List[Either[Exception,String]] = {
+    val map = Map(1 -> "January", 2 -> "February", 3 -> "March", 4 -> "April", 5 -> "May", 6 -> "June", 7 -> "July", 8 -> "August", 9 -> "September", 10 -> "October", 11 -> "November", 12 -> "December")
+    val list = num.map(n => map.get(n)).toList
+    list.map(_.toRight(new RuntimeException("Illegal number")))
+  }
 
-  // これは違う、アプリカティブなので今後
-  //  def num2Month4(num: Int): ValidationNel[Exception, String] = {
-  //    Map(1 -> "January", 2 -> "February", 3 -> "March", 4 -> "April", 5 -> "May", 6 -> "June", 7 -> "July", 8 -> "August", 9 -> "September", 10 -> "October", 11 -> "November", 12 -> "December")
-  //      .get(num)
-  //      .successNel[String]
-  //    (new Exception("Illegal number")).failureNel[Exception]
-  //  }
-
-  //  def month2Initial5(num: Int): String =
-  //    num2Month4(num)
-  //      .map(_.substring(0, 3))
-  //      .map(month => if (month == "May") month else s"$month.")
-  //      .getOrElse(throw new RuntimeException)
+  def months2Initial(num: Int*): String = {
+    numbers2Month(num: _*)
+      .map(_.map(_.substring(0 ,3))
+        .map(month => if (month == "May") month else s"$month.")
+        .getOrElse(throw new RuntimeException("Illegal number"))
+      ).toString()
+  }
 }
